cb51ca1b29e63a2b31ced4c800b654fe46d71b3a
naveen <naveenchowdaryn1209@gmail.com>
2020-02-15 20:16:10 +0530

    commited task2

--- cleancode/src/module-info.java
+++ cleancode/src/module-info.java
@@ -1,39 +1,40 @@
-import java.util .*;
-class sici
+import java.util.*;
+class constructioncost
 {
-double pr, rate, t, SI,CI;
-public void simpleinterest(double x,double y, double z)
+String s;
+int area;
+public void constructioncost(String x,int y)
 {
-pr=x;
-rate=y;
-t=z;
-SI=(pr * t * rate)/100;
-System.out.format("the simple interest is:"+SI);
-}
-public void compoundinterest(double x,double y,double z)
+s=x;
+area=y;
+switch(s)
 {
-pr=x;
-rate=y;
-t=z;
-CI=pr * Math.pow(1.0+rate/100.0,t) - pr;
-System.out.format("\n the Compound interest is:"+CI);
+case "standard":System.out.format("Total house construction cost if we use standard materials is:"+(1200*area)+"INR");
+                              break;
+
+case "above standard":System.out.format("Total house construction cost if we use above standard materials is:"+(1500*area)+"INR");
+                                          break;
+case "high standard":System.out.format("Total house construction cost if we use high standard materials is:"+(1800*area)+"INR");
+                                          break;
+case "fully automated":System.out.format("Total house construction cost if we use high standard materials and fully automated home is:"+(2500*area)+"INR");
+                                          break;
+default:System.out.format("enter the material standard as it is mentoined");
+                break;
 }
 }
-class interests
+}
+class house
 {
 public static void main(String args[])
 {
-    double principal, rateofinterest, time, SI,CI;
-     sici  obj=new sici();
-    Scanner sc=new Scanner (System. in);
-    System.out.println("Enter the amount:");
-    principal=sc.nextDouble();
-    System. out. println("Enter the No.of years:");
-    time=sc.nextDouble();
-    System. out. println("Enter the Rate of  interest");
-    rateofinterest=sc.nextDouble();
-    obj.simpleinterest(principal,rateofinterest,time);
-    obj.compoundinterest(principal,rateofinterest,time);
-    
-   }
+constructioncost obj=new constructioncost();
+Scanner sc=new Scanner(System.in);
+System.out.format("Select the material standard:\n1.standard\n2.above standard\n3.high standard\n4.fully automated");
+System.out.format("\nenter ur choice in words:");
+String standard=sc.nextLine();
+System.out.format("enter total area of house in Square fts:");
+int Totarea=sc.nextInt();
+obj.constructioncost(standard,Totarea);
 }
+}
+
